004137a86e11caff8f59b83ef7e66d80d986ef33
Fernando Mendes <fernando@mendes.codes>
2021-02-18 09:41:22 +0000

    Mock requests for data (#11)
    Adds a new method to request data from the user. For now it's returning
    hard-coded mock data.

--- src/contentScript/index.js
+++ src/contentScript/index.js
@@ -4,6 +4,9 @@ import ContentScriptConnection from "@sdk/Connection/ContentScriptConnection";
 
 import { injectScript } from "./injector";
 
+// TODO: replace this by a call to the extension;
+const FakeUser = { email: "user@example.org", name: "A User" };
+
 const sdk = chrome.runtime.getURL("sdk.bundle.js");
 injectScript(sdk);
 
@@ -12,4 +15,8 @@ const stream = new ContentScriptConnection();
 stream.on("verifyConnection", () => {
   const { version } = chrome.runtime.getManifest();
   return version;
+});
+
+stream.on("request", (field) => {
+  return FakeUser[field];
 });
--- src/sdk/Attester/index.js
+++ src/sdk/Attester/index.js
@@ -3,7 +3,8 @@ import InpageConnection from "@sdk/Connection/InpageConnection";
 const stream = new InpageConnection();
 
 const verifyConnection = () => stream.invoke("verifyConnection");
+const request = (field) => stream.invoke("request", [field]);
 
-const Attester = { verifyConnection };
+const Attester = { verifyConnection, request };
 
 export default Attester;
